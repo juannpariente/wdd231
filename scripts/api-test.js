691567a2da1b908b762ac71af063d04da3e3c4d6
juannpariente <jpariente000@gmail.com>
2025-10-02 08:16:39 -0300

    update the key

--- scripts/api-test.js
+++ scripts/api-test.js
@@ -2,17 +2,21 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const imgDesc = document.querySelector("figcaption");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&units=metric&appid=";
+const lat = 49.75; 
+const lon = 6.63;
+const APIkey = "da1cf4da2ea3223e6d41daeea6cf2122";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${APIkey}`;
 
 async function apiFetch() {
   try {
     const response = await fetch(url);
     if (response.ok) {
-        const data = await response.json()
-        console.log(data);
-        displayResults(data);
+      const data = await response.json()
+      console.log(data);
+      displayResults(data);
     } else {
-        throw Error(await response.text())
+      throw Error(await response.text())
     }
   } catch (error) {
     console.log(error);
@@ -22,10 +26,10 @@ async function apiFetch() {
 apiFetch()
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
 
-    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-    weatherIcon.setAttribute("alt", `weather icon`);
+  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+  weatherIcon.setAttribute("alt", `weather icon`);
 
-    imgDesc.textContent = `${data.weather[0].description}`
+  imgDesc.textContent = `${data.weather[0].description}`
 }
